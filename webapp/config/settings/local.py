7031ad68794c89a562faeff04cb8d5489c83db10
jslee <dlwhdtjd098@naver.com>
2024-04-17 21:34:58 +0900

    modify: Django 4 template 수정

--- webapp/config/settings/local.py
+++ webapp/config/settings/local.py
@@ -3,18 +3,26 @@ from .base import *
 DEBUG = True
 
 INSTALLED_APPS += [
-    'debug_toolbar',
+    "debug_toolbar",
 ]
-SECRET_KEY = os.environ.get("SECRET_KEY", "94n7fx27pd-!stt2fl@we!mn5=+-8l#!kber_j&p4s9hs+5w")
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware", ]
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "94n7fx27pd-!stt2fl@we!mn5=+-8l#!kber_j&p4s9hs+5w"
+)
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("NAME", "postgres"),
+        "USER": os.environ.get("USER", "postgres"),
+        "PASSWORD": os.environ.get("PASSWORD", "postgres"),
+        "HOST": os.environ.get("HOST", "localhost"),
+        "PORT": os.environ.get("PORT", "5432"),
     }
 }
--- webapp/config/settings/test.py
+++ webapp/config/settings/test.py
@@ -2,10 +2,17 @@ from .base import *
 
 DEBUG = True
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "94n7fx27pd-!stt2fl@we!mn5=+-8l#!kber_j&p4s9hs+5w")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "94n7fx27pd-!stt2fl@we!mn5=+-8l#!kber_j&p4s9hs+5w"
+)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("NAME", "test_db"),
+        "USER": os.environ.get("USER", "postgres"),
+        "PASSWORD": os.environ.get("PASSWORD", "postgres"),
+        "HOST": os.environ.get("HOST", "localhost"),
+        "PORT": os.environ.get("PORT", "5432"),
     }
 }
